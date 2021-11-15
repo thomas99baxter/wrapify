ab4a675180d684fec9ab145230c918d0f95f93d7
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-11-15 16:25:28 +0000

    chore: variable and stuff

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ app.listen(8080, ()=>{
 });
 
 app.get('/', (req, res) => {
-  res.render("index.ejs")
+  let artists = [ 
+    {name: 'Deadmau5', song: 'Ghosts n stuff'},
+     {name: 'Delta sleep', song: 'Camp adventure'}, 
+     {name: 'Camelphat' , song:'Breathe'}, 
+     {name: 'Disclosure', song: 'white noise'}, 
+     {name: 'Bicep', song: 'Glue'}
+  ]
+  let slogan = 'Spotify wrapped whenever'
+  res.render("index.ejs", {artists:artists})
 });
