c7d913953b049a332cc34bc0acf2f267c68c9dd4
Alex Wintle <alexwintle663@gmail.com>
2021-11-15 16:06:48 +0000

    feat: homepage navbar etc

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 
 app.listen(8080, ()=>{
   console.log("listening on post 8080")
 });
 
 app.get('/', (req, res) => {
-  res.send("hi")
+  res.render("index.ejs")
 });
