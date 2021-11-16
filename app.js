65336c96798444086e50bbbb60c2560373fd9459
Tom Baxter <thomas99baxter@gmail.com>
2021-11-16 11:27:15 +0000

    feat: implement auth

--- app.js
+++ app.js
@@ -10,35 +10,43 @@ let spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.CLIENT_SECRET,
   redirectUri: 'http://localhost:8080/test/'
 });
-console.log(spotifyApi)
-var code = process.env.CLIENT_ID;
 
-// Retrieve an access token and a refresh token
-spotifyApi.authorizationCodeGrant(code).then(
-  function(data) {
-    console.log('The token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
-    console.log('The refresh token is ' + data.body['refresh_token']);
 
-    // Set the access token on the API object to use it in later calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
-  },
-  function(err) {
-    console.log('Something went wrong!', err);
-  }
-);
+let code = 'AQBp4aKQqa91z3Ws7zSQQGDOEHnCbVXI_lLrYmzrVtTomgJANczUxpW2NbCGbpXC-JDLZm0xSjaNVBAoch7Ej1VGnoqi4SOJIxxTHh0bEI4neQIVilhIulR44smA24-AgPAkNhhOaJIGVOkDrmjd-ij-3-i7ikSZNqm5RYbcH5YPTZyYupgXcHx4uOeVkp4ZSy4-O3fgXzjOESEwNN5U2ykNnA'
+
+
+app.get('/login', (req, res) => {
+  var scopes = ['user-read-private', 'user-read-email'];
+  var state = 'some-state-of-my-choice';
+  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+  res.redirect(authorizeURL)
+});
 
 app.listen(8080, ()=>{
   console.log("listening on post 8080")
 });
+
 app.get('/test', (req, res) => {
+
+  spotifyApi.authorizationCodeGrant(req.query.code).then(
+    function(data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
+  
+      // Set the access token on the API object to use it in later calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
+    },
+    function(err) {
+      console.log('Something went wrong!', err);
+    }
+  );
   res.send("hello!")
 });
 
-app.get('/login', (req, res) => {
 
-});
 
 app.get('/', (req, res) => {
   let artists = [ 
