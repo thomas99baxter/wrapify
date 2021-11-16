cfc8e418b475921bc46b2bdacf6ab1b87b36f8e6
Tom Baxter <thomas99baxter@gmail.com>
2021-11-16 10:41:56 +0000

    add env variables

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require('dotenv').config()
+const querystring = require("querystring")
 const app = express();
 const SpotifyWebApi = require('spotify-web-api-node');
 
@@ -10,10 +12,42 @@ let spotifyApi = new SpotifyWebApi({
   redirectUri: 'http://www.example.com/callback'
 });
 
+function generateRandomString(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
 spotifyApi.setAccessToken('<your_access_token>');
 
 app.listen(8080, ()=>{
   console.log("listening on post 8080")
+});
+app.get('/test', (req, res) => {
+  res.send("hello!")
+});
+
+var client_id = process.env.CLIENT_ID;
+var redirect_uri = 'http://localhost:8080/test/';
+
+app.get('/login', (req, res) => {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
 });
 
 app.get('/', (req, res) => {
