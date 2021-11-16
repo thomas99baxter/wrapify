d4ebfd80ede09b914624890198333f4cee196906
Stella Martin <stellamartin222@gmail.com>
2021-11-16 10:17:42 +0000

    spotify wrapper installed

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+
 app.set("view engine", "ejs");
+
+let spotifyApi = new SpotifyWebApi({
+  clientId: '55fcc051d87741109912921021e65440',
+  clientSecret: '1da6e67941dc4cd8b5a7aabfe72404a2',
+  redirectUri: 'http://www.example.com/callback'
+});
+
+spotifyApi.setAccessToken('<your_access_token>');
 
 app.listen(8080, ()=>{
   console.log("listening on post 8080")
