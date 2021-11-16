2ed3da2e3f29bf6ae47f183120386e25059c2f58
Alex Wintle <alexwintle663@gmail.com>
2021-11-16 11:41:14 +0000

    feat(app.js): get an album

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const SpotifyWebApi = require('spotify-web-api-node');
 app.set("view engine", "ejs");
 
 let spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: 'http://localhost:8080/test/'
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'http://localhost:8080/test/'
 });
 
 
@@ -16,46 +16,57 @@ let code = 'AQBp4aKQqa91z3Ws7zSQQGDOEHnCbVXI_lLrYmzrVtTomgJANczUxpW2NbCGbpXC-JDL
 
 
 app.get('/login', (req, res) => {
-  var scopes = ['user-read-private', 'user-read-email'];
-  var state = 'some-state-of-my-choice';
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+    var scopes = ['user-read-private', 'user-read-email'];
+    var state = 'some-state-of-my-choice';
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
-  res.redirect(authorizeURL)
+    res.redirect(authorizeURL)
 });
 
-app.listen(8080, ()=>{
-  console.log("listening on post 8080")
+app.listen(8080, () => {
+    console.log("listening on post 8080")
 });
 
 app.get('/test', (req, res) => {
 
-  spotifyApi.authorizationCodeGrant(req.query.code).then(
-    function(data) {
-      console.log('The token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-      console.log('The refresh token is ' + data.body['refresh_token']);
-  
-      // Set the access token on the API object to use it in later calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-      spotifyApi.setRefreshToken(data.body['refresh_token']);
-    },
-    function(err) {
-      console.log('Something went wrong!', err);
-    }
-  );
-  res.send("hello!")
-});
+    spotifyApi.authorizationCodeGrant(req.query.code).then(
+        function (data) {
+            console.log('The token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
+            console.log('The refresh token is ' + data.body['refresh_token']);
 
+            // Set the access token on the API object to use it in later calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.setRefreshToken(data.body['refresh_token']);
+        },
+        function (err) {
+            console.log('Something went wrong!', err);
+        }
+    );
+
+    res.send("hello!")
+});
 
 
 app.get('/', (req, res) => {
-  let artists = [ 
-    {name: 'Deadmau5', song: 'Ghosts n stuff'},
-     {name: 'Delta sleep', song: 'Camp adventure'}, 
-     {name: 'Camelphat' , song:'Breathe'}, 
-     {name: 'Disclosure', song: 'white noise'}, 
-     {name: 'Bicep', song: 'Glue'}
-  ]
-  let slogan = 'Spotify wrapped whenever'
-  res.render("index.ejs", {artists:artists})
+    let artists = [
+        {name: 'Deadmau5', song: 'Ghosts n stuff'},
+        {name: 'Delta sleep', song: 'Camp adventure'},
+        {name: 'Camelphat', song: 'Breathe'},
+        {name: 'Disclosure', song: 'white noise'},
+        {name: 'Bicep', song: 'Glue'}
+    ]
+    let slogan = 'Spotify wrapped whenever'
+    res.render("index.ejs", {artists: artists})
 });
+
+app.get('/albums', ((req, res) => {
+
+    spotifyApi.getAlbum('5U4W9E5WsYb2jUQWePT8Xm')
+        .then(function (data) {
+            console.log('Album information', data.body);
+        }, function (err) {
+            console.error(err);
+        });
+
+}));
