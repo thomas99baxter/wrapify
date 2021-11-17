d22e701003a75d8f149dbd9b7550929b41a35f08
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-11-17 13:50:25 +0000

    chore: Added tests and comment explainations

--- test/getAlbums.test.js
+++ test/getAlbums.test.js
@@ -1,19 +1,26 @@
 const {expect} = require('chai');
 const sinon = require('sinon');
+// Mock responses from spotify api
 const { threeUniqueAlbumsMock, twoUniqueTwoDuplicateAlbumsMock} = require('./helpers/mocks/getAlbumsMocks');
 const { getMostListenedToAlbum } = require('../lib/getAlbums');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 describe('getMostListenedToAlbum', () => {
+  // Initialise stub variable so it can be accessed in the tests
   let spotifyApiStub;
 
+  // Create a fresh stub for each test
   beforeEach(() => {
+    // Mock out the spotify api library
+    // In this case we only use getMyTopTracks method
     spotifyApiStub = sinon.createStubInstance(SpotifyWebApi, {
       getMyTopTracks: sinon.stub().withArgs({
         time_range: "long_term",
         limit: "10",
+        // API returns a promise so we have to return a promise after out stub
       }).resolves({
         body: {
+          // Pass in our mocked response
           items: twoUniqueTwoDuplicateAlbumsMock,
         }
       }),
@@ -21,16 +28,24 @@ describe('getMostListenedToAlbum', () => {
   });
 
   afterEach(() => {
+    // Refresh all stubs
     sinon.restore();
   });
 
   it('should call getMyTopTracks method once', () => {
     getMostListenedToAlbum(spotifyApiStub);
+    // Expect spotifyApiStub to have been called once
     sinon.assert.calledOnce(spotifyApiStub.getMyTopTracks)
   });
 
-  it.only('should return an object', async () => {
+  it('should return an object', async () => {
     result = await getMostListenedToAlbum(spotifyApiStub);
-    console.log(result);
+    expect(result).to.be.an.instanceOf(Object)
+  });
+
+  it('should return an object with correct album value', async () => {
+    result = await getMostListenedToAlbum(spotifyApiStub);
+    // Use deep equal here top stop javascript messing up object equality
+    expect(result).to.deep.equal({"Techno Disco Tool": 2})
   });
 });
