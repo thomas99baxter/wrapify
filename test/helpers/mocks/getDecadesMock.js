a03d22cb846566bdbb5f3f854cb066b9e2a4dda9
Tom Baxter <thomas99baxter@gmail.com>
2021-11-23 11:51:15 +0000

    break the tests

--- test/helpers/mocks/getDecadesMock.js
+++ test/helpers/mocks/getDecadesMock.js
@@ -119,6 +119,36 @@ const topDecadeMockResponse = [
     type: 'track',
     uri: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
   },
+  {
+    album: {
+      album_type: 'SINGLE',
+      artists: [{name: "Mella Dee"}],
+      href: 'https://api.spotify.com/v1/albums/2lp65IrqbdOdbygnaXzMqv',
+      id: '2lp65IrqbdOdbygnaXzMqv',
+      images: [{
+        height: 640,
+        url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
+        width: 640
+      }],
+      name: 'Techno Drill Tool',
+      release_date: '1993-11-29',
+      type: 'album',
+      uri: 'spotify:album:2lp65IrqbdOdbygnaXzMqv'
+    },
+    artists: [ [Object] ],
+    external_urls: {
+      spotify: 'https://open.spotify.com/track/4dGWqeULKD767yx9NIMcEv'
+    },
+    href: 'https://api.spotify.com/v1/tracks/4dGWqeULKD767yx9NIMcEv',
+    id: '4dGWqeULKD767yx9NIMcEv',
+    is_local: false,
+    name: 'Techno Soul Tool',
+    popularity: 43,
+    preview_url: 'https://p.scdn.co/mp3-preview/7da26edea6e078e23d638c494b3770cba0e4bcaf?cid=e913603211e2465ebc852a64adaff635',
+    track_number: 1,
+    type: 'track',
+    uri: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
+  }
 ]
 
 
