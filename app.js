804043491b04c300a9f8a4afbc3c11375350e5a1
Alex Wintle <alexwintle663@gmail.com>
2021-11-23 16:36:11 +0000

    styling(*): highlight current time range complete:

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get('/view', async (req, res) => {
 
     res.render("index.ejs", {
         // favourite song info
+        time_range: TIME_RANGE,
         topSongName: songResult[0].song_name,
         topSongCover: songResult[0].song_cover,
         topSongs: songResult,
