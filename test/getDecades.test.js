cca468b5936ea9d72ea60cee217db68cdcbf102b
Tom Baxter <thomas99baxter@gmail.com>
2021-11-23 11:45:01 +0000

    test: fix coverage

--- test/getDecades.test.js
+++ test/getDecades.test.js
@@ -45,52 +45,59 @@ describe('getDecade', () => {
 
     it('should return a pretty object', async () => {
       let result = await getMostListenedToDecade(spotifyApiStub);
-      expect(result).to.deep.equal([
-        {
-          releaseDate: '2017-11-29',
-          releaseDecade: 2010,
-          songName: 'Techno Disco Tool',
-          image: {
-            height: 640,
-            url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
-            width: 640
-          },
-          link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
-        },
-        {
-          releaseDate: '2020-11-29',
-          releaseDecade: 2020,
-          songName: 'Techno Jazz Tool',
-          image: {
-            height: 640,
-            url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
-            width: 640
-          },
-          link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
-        },
-        {
-          releaseDate: '1984-11-29',
-          releaseDecade: 1980,
-          songName: 'Techno Funk Tool',
-          image: {
-            height: 640,
-            url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
-            width: 640
-          },
-          link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
-        },
-        {
-          releaseDate: '1993-11-29',
-          releaseDecade: 1990,
-          songName: 'Techno Soul Tool',
-          image: {
-            height: 640,
-            url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
-            width: 640
-          },
-          link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
-        }
-      ]
+      expect(result).to.deep.equal({
+        '1980': [
+          {
+            releaseDate: '1984-11-29',
+            releaseDecade: '1980',
+            songName: 'Techno Funk Tool',
+            image: {
+              height: 640,
+              url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
+              width: 640
+            },
+            link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
+          }
+        ],
+        '1990': [
+          {
+            releaseDate: '1993-11-29',
+            releaseDecade: '1990',
+            songName: 'Techno Soul Tool',
+            image: {
+              height: 640,
+              url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
+              width: 640
+            },
+            link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
+          }
+        ],
+        '2010': [
+          {
+            releaseDate: '2017-11-29',
+            releaseDecade: '2010',
+            songName: 'Techno Disco Tool',
+            image: {
+              height: 640,
+              url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
+              width: 640
+            },
+            link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
+          }
+        ],
+        '2020': [
+          {
+            releaseDate: '2020-11-29',
+            releaseDecade: '2020',
+            songName: 'Techno Jazz Tool',
+            image: {
+              height: 640,
+              url: 'https://i.scdn.co/image/ab6761610000e5ebef86feb3e2b9c57897ddc453',
+              width: 640
+            },
+            link: 'spotify:track:4dGWqeULKD767yx9NIMcEv'
+          }
+        ]}
       )
     });
 
